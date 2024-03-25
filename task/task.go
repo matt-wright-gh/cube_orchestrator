c54d66679890bec1f2335edc72dd5ae8442b83b6
Matt Wright <matthew.wright.3279@gmail.com>
2024-03-25 16:17:45 -0500

    Second chapter

--- task/task.go
+++ task/task.go
@@ -1,4 +1,10 @@
-package task 
+package task
+
+import (
+	"github.com/docker/go-connections/nat"
+	"github.com/google/uuid"
+	"time"
+)
 
 type State int
 
@@ -10,29 +16,23 @@ const (
 	Failed
 )
 
-import (
-	"time"
-	"github.com/google/uuid"
-	"github.com/docker/go-connections/nat"
-)
-
 type Task struct {
-	ID uuid.UUID
-	Name string
-	State State
-	Image string
-	Memory int
-	Disk int
-	ExposedPorts nat.PortSet
-	PortBindings map[string] string
+	ID            uuid.UUID
+	Name          string
+	State         State
+	Image         string
+	Memory        int
+	Disk          int
+	ExposedPorts  nat.PortSet
+	PortBindings  map[string]string
 	RestartPolicy string
-	StartTime time.Time
-	FinishTime time.Time
+	StartTime     time.Time
+	FinishTime    time.Time
 }
 
 type TaskEvent struct {
-	ID uuid.UUID
-	State State
+	ID        uuid.UUID
+	State     State
 	Timestamp time.Time
-	Task Task
+	Task      Task
 }
